73f6cc708677a121b6a3a73d8007f5eaaf133f84
Hima <himajyothsna@gmail.com>
2014-06-06 14:55:54 -0400

    Clean up formatting.

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
-    items:{ html:'<a href="https://help.rallydev.com/apps/2.0rc3/doc/">App SDK 2.0rc3 Docs</a>'},
+    items: { html: '<a href="https://help.rallydev.com/apps/2.0rc3/doc/">App SDK 2.0rc3 Docs</a>'},
+    
     launch: function() {
         console.log("My first App");
+        
         Ext.create('Rally.ui.dialog.ChooserDialog', {
-            artifactTypes: ['userstory','testfolder'],
+            artifactTypes: ['userstory', 'testfolder'],
             autoShow: true,
             height: 250,
             title: 'Choose User Stories',
             listeners: {
-                artifactChosen: function(selectedRecord){
+                artifactChosen: function(selectedRecord) {
                     Ext.Msg.alert('Chooser', selectedRecord.get('Name') + ' was chosen');
                 },
                 scope: this
